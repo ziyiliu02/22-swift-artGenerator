72fb50a06684f829ae10716684e202806d89af7e
ziyiliu02 <ziyi.liu02@gmail.com>
2024-01-08 11:06:16 +0800

    - Use a configuration file to store the API Key - Create models to submit an HTTP POST request with data to the API - Create APIService and fetch function - Verify the API works using the API Key

---
+++ 22-swift-artGenerator/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  22-swift-artGenerator
+//
+//  Created by Liu Ziyi on 7/1/24.
+//
+
+import Foundation
+
+enum Constants {
+    static let imageSize = "256x256"
+    static let n = 1
+}
+
+struct GenerationInput: Codable {
+    var prompt: String
+    var n = Constants.n
+    var size = Constants.imageSize
+    
+    var encodedData: Data? {
+        try? JSONEncoder().encode(self)
+    }
+}
