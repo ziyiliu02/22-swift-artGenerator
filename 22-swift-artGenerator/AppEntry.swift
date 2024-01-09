f289d68ad5b9aac6974417bc5265991bd9dd3237
ziyiliu02 <ziyi.liu02@gmail.com>
2024-01-09 09:12:58 +0800

    - File navigation organisation

--- 22-swift-artGenerator/AppEntry.swift
+++ 22-swift-artGenerator/AppEntry.swift
@@ -11,16 +11,7 @@ import SwiftUI
 struct AppEntry: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .onAppear {
-//                    print(Bundle.main.infoDictionary?["API_KEY"] as? String ?? "")
-                    Task {
-                        let sample = GenerationInput(prompt: "Man in a rowboat in the ocean in a storm similar to work by Van Gogh")
-                        if let data = sample.encodedData {
-                            try await APIService().fetchImages(with: data)
-                        }
-                    }
-                }
+            DALLEImagesView()
         }
     }
 }
