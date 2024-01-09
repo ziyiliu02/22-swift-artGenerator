eff8d2187359c08d772c4e4a9152be3d8e8011b7
ziyiliu02 <ziyi.liu02@gmail.com>
2024-01-09 10:32:42 +0800

    - Create the ViewModel that will drive the view state

---
+++ 22-swift-artGenerator/Screens/DALLE Images/ViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  ViewModel.swift
+//  22-swift-artGenerator
+//
+//  Created by Liu Ziyi on 9/1/24.
+//
+
+import SwiftUI 
+
+@MainActor
+class ViewModel: ObservableObject {
+    @Published var prompt = ""
+    @Published var urls: [URL] = []
+    @Published var dallEImages: [DalleImage] = []
+    @Published var fetching = false
+    
+    let apiService = APIService()
+    
+    func clearProperties() {
+        urls = []
+        dallEImages.removeAll()
+        for _ in 1...Constants.n {
+            dallEImages.append(DalleImage())
+        }
+    }
+    
+    init() {
+        clearProperties()
+    }
+    
+    func fetchImages() {
+        clearProperties()
+        withAnimation {
+            fetching.toggle()
+        }
+        let generationInput = GenerationInput(prompt: prompt)
+        Task {
+            if let data = generationInput.encodedData {
+                do {
+                    let response = try await apiService.fetchImages(with: data)
+                    for data in response.data {
+                        urls.append(data.url)
+                    }
+                    withAnimation {
+                        fetching.toggle()
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                    fetching.toggle()
+                }
+            }
+        }
+    }
+}
